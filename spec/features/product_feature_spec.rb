2f3a068a24481197e81a8b965e3119213d9bd5eb
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-04 04:32:11 +0000

    Automatically backed up by Learn

--- spec/features/product_feature_spec.rb
+++ spec/features/product_feature_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-
+=begin
 RSpec.describe "Products", type: :feature do
   before do
     Product.destroy_all
@@ -27,3 +27,4 @@ RSpec.describe "Products", type: :feature do
     expect(page).to have_content p2.description
   end
 end
+=end
