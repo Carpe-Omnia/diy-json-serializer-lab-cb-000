0593138c9f8abf4665434111a2a8ebf5a706def7
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-04 04:15:51 +0000

    Automatically backed up by Learn

--- app/models/productserializer.rb
+++ app/models/productserializer.rb
@@ -1,4 +1,13 @@
 class ProductSerializer
   def initialize()
   end
+  def serialize(product)
+    json = "{"
+    json += "'name': '#{product.name}', "
+    json += "'price': '#{product.price}', "
+    json += "'inventory': '#{product.description}', "
+    json += "'description': '#{product.description}'"
+    json += "}"
+    return json
+  end
 end
